a85924724273f18391c25a60d817594ab19b1a56
sero583 <serhatguler@hotmail.de>
2017-03-22 00:12:29 +0100

    Update GameEndEvent.php
    Revert useless implement of Cancellable and fix use'd classes + improve code

--- src/corytortoise/DuelsPE/events/GameEndEvent.php
+++ src/corytortoise/DuelsPE/events/GameEndEvent.php
@@ -13,16 +13,25 @@
   use pocketmine\Player;
 
   use corytortoise\DuelsPE\Main;
+  use corytortoise\DuelsPE\Arena;
 
-  class GameEndEvent extends PluginEvent implements Cancellable {
 
+  class GameEndEvent extends PluginEvent {
+    /** @var Main */
     private $plugin;
-
+    /** @var Player */
     private $winner;
+    /** @var Player */
     private $loser;
-
+    /** @var Arena */
     private $arena;
 
+   /**	
+     * @param Main $plugin
+     * @param Player            $winner
+     * @param Player          $looser
+     * @param Arena             $arena
+     */
     public function __construct(Main $plugin, Player $winner, Player $loser, Arena $arena) {
       parent::__construct($plugin);
       $this->plugin = $plugin;
@@ -31,16 +40,24 @@
       $this->arena = $arena;
     }
 
+    /**
+     * @return Player
+     **/
     public function getWinner() {
       return $this->winner;
     }
 
+    /**
+     * @return Player
+     **/
     public function getLoser() {
       return $this->loser;
     }
 
+    /**
+     * @return Arena
+     **/
     public function getArena() {
       return $this->arena;
     }
-
   }
